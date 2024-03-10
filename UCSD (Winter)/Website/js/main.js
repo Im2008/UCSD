230734a30f0e75b8a8ad4e6e8ad0119499721e3d
Ian <ianm02879@gmail.com>
2024-03-10 13:06:00 -0700

    stuff

--- UCSD (Winter)/Website/js/main.js
+++ UCSD (Winter)/Website/js/main.js
@@ -1,1 +1,39 @@
-alert("hello world")
+//Javascript Code:
+
+function usersYes() {
+    let user_prompt = prompt ("What is your name?");
+    var results = document.getElementById("mail")
+    results.innerHTML = user_prompt
+    alert("Confirmed. Thank you!");
+}
+
+function usersNo() {
+    alert("Confirmed. Thank you!");
+}
+
+function bigImage(big) {
+    big.style.height = "200px";
+    big.style.width = "200px";
+}
+
+function regularImage(regular) {
+    regular.style.height = "100px";
+    regular.style.width = "100px";
+}
+
+
+//jQuery Code:
+
+/* jslint devel: true */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/* eslint-env es6*/
+/* exported showAlert()*/
+/* eslint-disable no-console */
+/* eslint-env jquery*/
+/* jslint browser: true*/
+/* global  $*/
+
+$( document ).ready(function() {
+    $("h3").hide("slow");
+});
